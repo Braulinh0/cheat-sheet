9a97cd692417f9d12026dc845b21106b47ab5482
Braulio Peña <xxbraulioninjayt133@gmail.com>
2025-01-18 12:28:42 -0300

    Competitive main framework in C++

---
+++ C++ Basic/Main elements/main_struct.cpp
@@ -0,0 +1,17 @@
+#pragma GCC optimize("O2")
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define endl '\n'
+
+int main() {
+    ios::sync_with_stdio(0);
+    cout.tie(0);
+    cin.tie(0);
+    cout.setf(ios::fixed);
+    cout.precision(10);
+
+    
+
+    return 0;
+}
