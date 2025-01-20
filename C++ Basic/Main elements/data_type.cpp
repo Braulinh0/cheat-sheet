52678e6cbcf97d2548413632bf2b4fdd835225c1
Braulio Peña <xxbraulioninjayt133@gmail.com>
2025-01-19 21:21:31 -0300

    data_type.cpp

--- C++ Basic/Main elements/data_type.cpp
+++ C++ Basic/Main elements/data_type.cpp
@@ -12,13 +12,21 @@ int main() {
     cout.precision(10);
 
     char character; // un caracter
+    // --------------------------------------------------------------------------------------------
     int numeros_int; // números enteros hasta 10^9
+    // --------------------------------------------------------------------------------------------
     unsigned int numeros_unsigned_int; // números unicamente positivos hasta 2^32 - 1
+    // --------------------------------------------------------------------------------------------
     long long numeros_long_long; // números enteros hasta 10^18
+    // --------------------------------------------------------------------------------------------
     unsigned long long numeros_unsigned_long_long; // números unicamente positivos hasta 2^64 - 1
+    // --------------------------------------------------------------------------------------------
     float numeros_float; // número real precisión simple (7 dígitos de precisión)
+    // --------------------------------------------------------------------------------------------
     double numeros_double; // número real precisión doble (15 dígitos de precisión)
+    // --------------------------------------------------------------------------------------------
     string cadena; // cadena de caracteres
+    // --------------------------------------------------------------------------------------------
     bool booleano; // solo valores verdadero o falso (true = 1 o false = 0)
 
     return 0;
